84509df071cef4a0266d0987b3c5b9059d44ab6e
Rushad Antia <rushadantia@gmail.com>
2015-07-31 11:45:46 -0400

    added settings window

--- src/com/example/moddroid/MainActivity.java
+++ src/com/example/moddroid/MainActivity.java
@@ -1,24 +1,33 @@
 package com.example.moddroid;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
-	@Override
+
+	private Button settings;
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		settings = (Button)findViewById(R.id.settingsButton);
+		settings.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+			 startActivity(new Intent(getApplicationContext(),ModSettings.class));
+
+			}
+		});
+
+
 	}
-	
+
 
 }
---
+++ src/com/example/moddroid/ModSettings.java
@@ -0,0 +1,21 @@
+package com.example.moddroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ModSettings extends Activity {
+
+	 
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_mod_settings);
+	
+	
+	
+	
+	
+	
+	}
+
+	
+}
