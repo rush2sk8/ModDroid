e207e6a9fcd6baafc7b627dbc5d203f86f86746f
Rushad Antia <rushadantia@gmail.com>
2015-07-31 13:52:34 -0400

    working tudor rusu

--- gen/com/example/moddroid/R.java
+++ gen/com/example/moddroid/R.java
@@ -26,11 +26,12 @@ public final class R {
         public static final int tr=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080005;
+        public static final int editText1=0x7f080004;
         public static final int nameLabel=0x7f080000;
-        public static final int settingsButton=0x7f080001;
+        public static final int settingsButton=0x7f080002;
         public static final int tudorFace=0x7f080003;
-        public static final int viewButton=0x7f080002;
+        public static final int viewButton=0x7f080001;
     }
     public static final class layout {
         public static final int activity_camera=0x7f030000;
--- src/com/example/moddroid/MainActivity.java
+++ src/com/example/moddroid/MainActivity.java
@@ -5,12 +5,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 
 	private Button settings;
+	private TextView tv;
+	private ImageView iv;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,12 +29,69 @@ public class MainActivity extends Activity {
 
 			@Override
 			public void onClick(View v) {
-			 startActivity(new Intent(getApplicationContext(),ModSettings.class));
+				startActivity(new Intent(getApplicationContext(),ModSettings.class));
 
 			}
 		});
 
+		iv = (ImageView)findViewById(R.id.tudorFace);
+		tv = (TextView)findViewById(R.id.nameLabel);
+		tv.setOnClickListener(new OnClickListener() {
 
+			@Override
+			public void onClick(View v) {
+				Animation fade = new AlphaAnimation(0, 1);
+				fade.setInterpolator(new AccelerateInterpolator());
+				fade.setDuration(1000);
+				fade.setAnimationListener(new AnimationListener() {
+
+					@Override
+					public void onAnimationStart(Animation animation) {
+						// TODO Auto-generated method stub
+
+					}
+
+					@Override
+					public void onAnimationRepeat(Animation animation) {
+						// TODO Auto-generated method stub
+
+					}
+
+					@Override
+					public void onAnimationEnd(Animation animation) {
+						iv.setVisibility(View.INVISIBLE);
+
+					}
+				});
+				Animation fade1 = new AlphaAnimation(1,0);
+				fade1.setInterpolator(new AccelerateInterpolator());
+				fade1.setDuration(1000);
+
+				fade1.setAnimationListener(new AnimationListener() {
+
+					@Override
+					public void onAnimationStart(Animation animation) {
+						// TODO Auto-generated method stub
+
+					}
+
+					@Override
+					public void onAnimationRepeat(Animation animation) {
+						// TODO Auto-generated method stub
+
+					}
+
+					@Override
+					public void onAnimationEnd(Animation animation) {
+					iv.startAnimation(fade);
+
+					}
+				});
+				
+			
+				iv.startAnimation(fade1);
+			}
+		});
 	}
 
 
