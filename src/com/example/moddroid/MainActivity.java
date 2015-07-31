7597b80f1d128000220e360988a429b42babba1b
Pacopenguin <ldroepe@gmail.com>
2015-07-31 11:38:07 -0400

    asd

--- src/com/example/moddroid/MainActivity.java
+++ src/com/example/moddroid/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.moddroid;
 
 import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
 
 public class MainActivity extends Activity {
 
@@ -9,11 +18,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-
-
-
-
 	}
+	
 
 }
